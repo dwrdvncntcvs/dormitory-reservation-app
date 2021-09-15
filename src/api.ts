ced008d8af065761f6302df8caba9ad6404bec1c
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-09-15 11:43:54 +0800

    API Connection, Storage Initialization, Http Client Module Added

---
+++ src/api.ts
@@ -0,0 +1,4 @@
+export const api = {
+  url: 'http://localhost:3000',
+  ngrok_url: ''
+}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { api } from 'src/api';
+
+const api_url = api.url;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user: Observable<any>;
+  userData = new BehaviorSubject(null);
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, platform: Platform) {}
+
 }
