deb51a198abb6031e5c9e4ea1b3879ca12e3cade
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-09-16 23:43:40 +0800

    Dormitory Service Create as well as the request

--- src/app/pages/dormitory-list/dormitory-list.page.ts
+++ src/app/pages/dormitory-list/dormitory-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DormitoriesService } from 'src/app/services/dormitories.service';
+import { api } from 'src/api';
 
 @Component({
   selector: 'app-dormitory-list',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dormitory-list.page.scss'],
 })
 export class DormitoryListPage implements OnInit {
+  dormitoryData: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private dormitoriesService: DormitoriesService) {
+    this.getAllUserDormitories();
   }
 
+  url = api.url;
+
+  ngOnInit() {}
+
+  getAllUserDormitories() {
+    this.dormitoriesService.getAllUserDormitoriesRequest().then((response) => {
+      response.subscribe((data) => {
+        this.dormitoryData = data['userDormitories'];
+        console.log(this.dormitoryData);
+      });
+    });
+  }
 }
---
+++ src/app/services/dormitories.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
+import { from, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { api } from 'src/api';
+import { UserService } from './user.service';
+
+const api_url = api.url;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DormitoriesService {
+  constructor(
+    private httpClient: HttpClient,
+    private storage: Storage,
+    private router: Router
+  ) {}
+
+  async getAllUserDormitoriesRequest() {
+    const token = await this.storage.get('user_token');
+    console.log("Token: ", token);
+
+    const url = `${api_url}/view-owner-dormitories`;
+
+    return this.httpClient.get(url, {
+      headers: { Authorization: 'Bearer ' +  token },
+    });
+  }
+}
