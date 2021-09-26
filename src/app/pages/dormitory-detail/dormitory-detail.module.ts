8accfad0885dca7c3f6dc58c5d65bb16fd560525
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-09-26 14:00:14 +0800

    Dormitory Detail UI with Data

--- src/app/pages/dormitory-detail/dormitory-detail.module.ts
+++ src/app/pages/dormitory-detail/dormitory-detail.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { DormitoryDetailPageRoutingModule } from './dormitory-detail-routing.module';
 
 import { DormitoryDetailPage } from './dormitory-detail.page';
+import { SharedModule } from 'src/app/components/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    DormitoryDetailPageRoutingModule
+    DormitoryDetailPageRoutingModule,
+    SharedModule,
   ],
   declarations: [DormitoryDetailPage]
 })
--- src/app/pages/dormitory-detail/dormitory-detail.page.ts
+++ src/app/pages/dormitory-detail/dormitory-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DormitoriesService } from 'src/app/services/dormitories.service';
+import { api } from 'src/api';
 
 @Component({
   selector: 'app-dormitory-detail',
@@ -9,15 +10,23 @@ import { DormitoriesService } from 'src/app/services/dormitories.service';
 })
 export class DormitoryDetailPage implements OnInit {
   dormId: number;
+  dormitoryData: any;
+  url = api.url;
+  dormitoryStatus: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private dormitoriesService: DormitoriesService
+    private dormitoriesService: DormitoriesService,
+    private router: Router
   ) {
     this.getDormitoryDetail();
   }
 
   ngOnInit = () => {};
+
+  goBackToHome = () => {
+    this.router.navigate(['owner-tabs/dormitory-list'])
+  };
 
   getDormitoryDetail = () => {
     this.activatedRoute.paramMap.subscribe((params) => {
@@ -27,6 +36,13 @@ export class DormitoryDetailPage implements OnInit {
         .getDormitoryDetails(id)
         .subscribe((dormitoryData) => {
           console.log(dormitoryData);
+          this.dormitoryData = dormitoryData['dormitory'];
+          const status = this.dormitoryData.isAccepting;
+          if (status === true) {
+            this.dormitoryStatus = 'Active'
+          } else {
+            this.dormitoryStatus = 'Not Active'
+          }
         });
     });
   };
