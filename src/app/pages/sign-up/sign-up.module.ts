bf00b34f100a9c341ab6c3f9b09c95c89a4ddec5
Reneil Moncawe <reneil.moncawe@g.batstate-u.edu.ph>
2021-09-26 12:44:16 +0800

    only name and email has a validation

--- src/app/pages/sign-up/sign-up.module.ts
+++ src/app/pages/sign-up/sign-up.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
 import { SignUpPage } from './sign-up.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SignUpPageRoutingModule
+    ReactiveFormsModule,
+    SignUpPageRoutingModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: SignUpPage
+      }
+    ])
   ],
   declarations: [SignUpPage]
 })
