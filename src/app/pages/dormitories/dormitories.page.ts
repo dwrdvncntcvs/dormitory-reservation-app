cc99fcb70ba69ce97148bdebbd41178d72d82e56
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-11-04 22:57:23 +0800

    Display Filtered Dormitories Using Reservation Id Created

--- src/app/pages/dormitories/dormitories.page.ts
+++ src/app/pages/dormitories/dormitories.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DormitoriesService } from 'src/app/services/dormitories.service';
 import { api } from 'src/api';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-dormitories',
@@ -9,8 +10,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./dormitories.page.scss'],
 })
 export class DormitoriesPage implements OnInit {
+  filterDormitoriesByReservation = [];
+  userDormitoryReservations: any[];
   dormitoryData = [];
   haveDormitories: boolean;
+  userRole: string;
+
+  reservedDormitoriesToggle: boolean = false;
 
   url = api.url;
 
@@ -27,27 +33,50 @@ export class DormitoriesPage implements OnInit {
   };
 
   constructor(
+    private userService: UserService,
     private dormitoriesService: DormitoriesService,
     private router: Router
   ) {}
 
   ngOnInit() {}
 
-  segmentChange(event){
-    console.log(event.target.value)
+  segmentChange(event) {
+    console.log(event.target.value);
   }
 
   ionViewDidEnter = () => {
+    this.checkUserRole();
     this.getAllDormitories('all', '?');
   };
 
+  ionViewDidLeave = () => {
+    this.reservedDormitoriesToggle = false;
+    this.genderToggle = false;
+    this.priceToggle = false;
+    this.filterDormitoriesByReservation = [];
+  };
+
+  checkUserRole = async () => {
+    const userRole = await this.userService.checkUserRole();
+    this.userRole = userRole;
+  };
+
+  openReservedDormitoriesToggle = () => {
+    this.priceToggle = false;
+    this.genderToggle = false;
+    this.reservedDormitoriesToggle = true;
+    this.getUserDormitoryReservationAction();
+  };
+
   openGenderToggle = () => {
-    if (this.priceToggle === true) this.priceToggle = false;
+    this.priceToggle = false;
+    this.reservedDormitoriesToggle = false;
     this.genderToggle = !this.genderToggle;
   };
 
   openPriceToggle = () => {
-    if (this.genderToggle === true) this.genderToggle = false;
+    this.genderToggle = false;
+    this.reservedDormitoriesToggle = false;
     this.priceToggle = !this.priceToggle;
   };
 
@@ -66,6 +95,7 @@ export class DormitoriesPage implements OnInit {
     if (filter1 === 'all') {
       this.genderToggle = false;
       this.priceToggle = false;
+      this.reservedDormitoriesToggle = false;
     }
     this.dormitoriesService
       .getAllDormitoriesRequest(filter1, filter2)
@@ -74,6 +104,55 @@ export class DormitoriesPage implements OnInit {
         this.dormitoryData = response['dormitories'];
       });
   }
+
+  getUserDormitoryReservationAction = () => {
+    console.log("I'am Being Run");
+    this.dormitoriesService
+      .getUserDormitoryReservationRequest()
+      .then((response) => {
+        response.subscribe(
+          (responseData) => {
+            console.log(responseData);
+            const userDormitoryReservation = responseData['userReservation'];
+            this.userDormitoryReservations = userDormitoryReservation;
+            this.getDormitoriesById(userDormitoryReservation);
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+      });
+  };
+
+  getDormitoryByReservationIdAction = (reservationId: number) => {
+    console.log("Reservation Id: ", reservationId);
+
+  };
+
+  getDormitoriesById = (userReservations: any) => {
+    console.log('Reservation List', userReservations);
+    let dormitoryArr = [];
+    for (let reservation of userReservations) {
+      // this.getDormitoryByReservationIdAction(reservation.id);
+      this.dormitoriesService
+      .getDormitoryByReservationIdRequest(reservation.id)
+      .then((response) => {
+        response.subscribe(
+          (responseData) => {
+            const newDormitoryData = responseData['dormitoryData']
+            console.log("Dormitory Data: ", newDormitoryData)
+            dormitoryArr.push(newDormitoryData)
+          },
+          (err) => {
+            console.log(err);
+          }
+        );
+      });
+    }
+    console.log("Dormitory Arr: ", dormitoryArr)
+    this.filterDormitoriesByReservation = dormitoryArr;
+    console.log("Dormitory Arr2: ", this.filterDormitoriesByReservation)
+  };
 
   sliderOpts = {
     slidesPerView: 1.25,
