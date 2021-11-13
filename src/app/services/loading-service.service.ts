02fa4d79bc0d1d7f1c4ac2b229037854b5a992f6
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-11-13 13:27:20 +0800

    Loading Service Created

---
+++ src/app/services/loading-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingServiceService {
+
+  constructor(private loadingController: LoadingController) { }
+
+  createNewLoading = async (message: string) => {
+    const loading = await this.loadingController.create({
+      message,
+      spinner: 'circles'
+    });
+    loading.present();
+  }
+
+  dismissLoading = () => {
+    this.loadingController.dismiss();
+  }
+}
