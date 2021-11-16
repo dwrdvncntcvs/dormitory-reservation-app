cbfc0a8b78fdeee5b649cb9668dff22445e6d55b
Reneil Moncawe <reneil.moncawe@g.batstate-u.edu.ph>
2021-11-16 12:42:24 +0800

    reload design

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 export const api = {
+  url: 'http://localhost:3000'
   // url: 'https://b8b3-136-158-30-56.ngrok.io'
-  url: 'https://4ce2-136-158-30-154.ngrok.io',
+  // url: 'https://4ce2-136-158-30-154.ngrok.io',
 };
