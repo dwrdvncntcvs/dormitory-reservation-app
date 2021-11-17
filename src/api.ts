6896c3d12bc3e8d9cb7e2394cd6185bcd994b305
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-11-17 12:02:14 +0800

    API Changed

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,3 @@
 export const api = {
-  url: 'http://localhost:3000'
-  // url: 'https://b8b3-136-158-30-56.ngrok.io'
-  // url: 'https://4ce2-136-158-30-154.ngrok.io',
+  url: 'https://dormitory-reservation-api.herokuapp.com'
 };
