63888d30220ee50decb4f7568779eebd0f5cb55e
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-11-22 13:29:53 +0800

    Pending Reservation Detail Badge Created

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
 export const api = {
-  // url: 'https://dormitory-reservation-api.herokuapp.com'
-  url: 'http://localhost:3000'
+  url: 'https://dormitory-reservation-api.herokuapp.com'
+  // url: 'http://localhost:3000'
 };
