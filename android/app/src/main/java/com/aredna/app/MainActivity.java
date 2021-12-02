0fda4868b32b2e1d5ecc87a7176f98d0aae48b67
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2021-12-02 19:29:19 +0800

    Last Change

--- android/app/src/main/java/com/aredna/app/MainActivity.java
+++ android/app/src/main/java/com/aredna/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.aredna.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.aredna.app',
   appName: 'aredna',
   webDir: 'www',
   bundledWebRuntime: false
